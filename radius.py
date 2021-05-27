6fa71987253ce9ec28faf8662d6aa68ce29558f4
Hardikk106 <83875949+Hardikk106@users.noreply.github.com>
2021-05-27 13:03:29 +0530

    radius.py
    project no 6

---
+++ radius.py
@@ -0,0 +1,13 @@
+def radius():
+     """i havent use math because i dont know how to use""" 
+pie = 3.14
+
+str_rds = "Please Enter your radius:"
+radius = int(input(Str_rds))
+
+print(f"Your radius is {radius}")
+
+print("the area of your circle is")
+
+area = radius*radius*pie    
+print(area)
